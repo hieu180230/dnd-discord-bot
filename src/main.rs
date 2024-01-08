74f01c9d5005c3f4dd75e165a9ffef78b31b8a58
hieu18022004 <hieu180230@gmail.com>
2024-01-08 21:24:13 +0700

    move token to an outer file to be collected

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
     unused_labels,
     non_snake_case
 )]
+use dotenv::dotenv;
 use std::env;
 use std::sync::Arc;
 use serenity::{
@@ -28,9 +29,13 @@ use std::collections::HashMap;
 async fn main() {
     //init the resource list
     if !RESOURCES_LIST.is_empty(){println!("ok loading data");}
-    else {println!("loading data");}
+
     //discord bot token
-    env::set_var("DISCORD_TOKEN", "MTE4MDgxMzM0MTk0NjU2MDU0Mg.Gesusw.IpfDTthqaJGcjMyVc_ow5yjSUuJYAkfkn7l7Ls");
+    //load environment variable from .env file
+    dotenv::dotenv().expect("Failed to load .env file");
+    //initiate the logger to use environment variable
+    tracing_subscriber::fmt::init(); //need to read document
+
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
@@ -40,6 +45,7 @@ async fn main() {
         .group(&DND_GROUP);
     framework.configure(Configuration::new().prefix("!"));
 
+    //this build the client with Handler and frame
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .framework(framework)
@@ -50,6 +56,13 @@ async fn main() {
         let mut data = client.data.write().await;
         data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
     }
+
+    //clone the shard manager and will shut down along with main program
+    let shard_manager = client.shard_manager.clone();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.shutdown_all().await;
+    });
 
     if let Err(why) = client.start().await {
         println!("Client error: {why:?}");
